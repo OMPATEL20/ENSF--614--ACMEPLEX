3838a64726eace15f30aa2ba52a5e365ab55e26a
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2024-12-01 10:10:10 -0700

    Update SeatController.java

--- src/main/java/com/acmeplex/controller/SeatController.java
+++ src/main/java/com/acmeplex/controller/SeatController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ import com.acmeplex.service.PaymentService;
 import com.acmeplex.service.SeatService;
 import com.acmeplex.service.TicketcancelService;
 
-@RestController
+@Controller
 @RequestMapping
 public class SeatController {
 
