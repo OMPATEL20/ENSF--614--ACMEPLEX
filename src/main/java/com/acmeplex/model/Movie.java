e96d9f04b8fb804e0272a28c70c28ca5f4ee2d7d
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2024-12-01 09:57:39 -0700

    Update Movie.java

--- src/main/java/com/acmeplex/model/Movie.java
+++ src/main/java/com/acmeplex/model/Movie.java
@@ -1,7 +1,5 @@
 package com.acmeplex.model;
 
-
-
 import java.time.LocalDate;
 
 public class Movie {
@@ -12,7 +10,8 @@ public class Movie {
     private LocalDate releaseDate;
 
     // Constructors
-    public Movie() {}
+    public Movie() {
+    }
 
     public Movie(int movieId, String title, double rating, Theater theater, LocalDate releaseDate) {
         this.movieId = movieId;
@@ -74,106 +73,3 @@ public class Movie {
                 '}';
     }
 }
-
-
-// package com.acmeplex.models;
-
-// import java.time.LocalDate;
-// import java.util.ArrayList;
-
-// /**
-//  * Represents a movie in the system
-//  */
-// public class Movie {
-//     private int movieId;
-//     private String title;
-//     private LocalDate releaseDay;
-//     private ArrayList<Showtime> showtimes;
-//     private double rating;
-
-//     // Constructors
-//     public Movie() {}
-
-//     public Movie(String title, int movieId, double rating, LocalDate releaseDay) {
-//         this.title = title;
-//         this.movieId = movieId;
-//         this.rating = rating;
-//         this.releaseDay = releaseDay;
-//         this.showtimes = new ArrayList<>();
-//     }
-
-//     // Methods for managing showtimes
-//     public void addShowTimes(Showtime showtime) {
-//         if (this.movieId != showtime.getMovieId()) {
-//             System.out.println("movieId must match before adding");
-//             return;
-//         }
-//         if (!this.showtimes.stream().anyMatch(t -> t.getTime().equals(showtime.getTime()))) {
-//             this.showtimes.add(showtime);
-//         }
-//     }
-
-//     public void removeShowTimes(Showtime showtime) {
-//         if (this.movieId != showtime.getMovieId()) {
-//             System.out.println("movieId must match before removing");
-//             return;
-//         }
-//         if (this.showtimes.parallelStream().anyMatch(t -> t.getTime().equals(showtime.getTime()))) {
-//             this.showtimes.remove(showtime);
-//         }
-//     }
-
-//     // Getters and setters
-//     public ArrayList<Showtime> getShowtimes() {
-//         return showtimes;
-//     }
-
-//     public void setShowtimes(ArrayList<Showtime> showtimes) {
-//         this.showtimes = showtimes;
-//     }
-
-//     public String getTitle() {
-//         return title;
-//     }
-
-//     public void setTitle(String title) {
-//         this.title = title;
-//     }
-
-//     public int getMovieId() {
-//         return movieId;
-//     }
-
-//     public void setMovieId(int movieId) {
-//         this.movieId = movieId;
-//     }
-
-//     public double getRating() {
-//         return rating;
-//     }
-
-//     public void setRating(double rating) {
-//         this.rating = rating;
-//     }
-
-//     public LocalDate getReleaseDay() {
-//         return releaseDay;
-//     }
-
-//     public void setReleaseDay(LocalDate releaseDay) {
-//         this.releaseDay = releaseDay;
-//     }
-
-//     @Override
-//     public boolean equals(Object o) {
-//         if (this == o) return true;
-//         if (o == null || getClass() != o.getClass()) return false;
-//         Movie movie = (Movie) o;
-//         return movieId == movie.movieId;
-//     }
-
-//     @Override
-//     public int hashCode() {
-//         return Integer.hashCode(movieId);
-//     }
-// }
