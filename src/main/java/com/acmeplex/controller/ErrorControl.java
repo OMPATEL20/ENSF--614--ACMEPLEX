cda8b55547809d524f2b6e6c56f011dafb6346eb
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2024-12-01 09:46:49 -0700

    Update ErrorControl.java

--- src/main/java/com/acmeplex/controller/ErrorControl.java
+++ src/main/java/com/acmeplex/controller/ErrorControl.java
@@ -1,8 +1,7 @@
 package com.acmeplex.controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,26 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorControl implements ErrorController {
-	// @RequestMapping(path="/error", method = RequestMethod.POST)
-	// public String handleError() {
-	// return "error";
-	// }
-	@GetMapping(value = "/error")
-	public String handleError(HttpServletRequest request) {
 
+	@GetMapping("/error")
+	public String handleError(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
 		if (status != null) {
-
-			Integer statusCode = Integer.valueOf(status.toString());
+			int statusCode = Integer.parseInt(status.toString());
 
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
-				return "error404";
+				return "error404"; // Custom 404 page
 			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-				return "error500";
+				return "error500"; // Custom 500 page
 			}
 		}
-		return "error";
+		return "error"; // Fallback error page
 	}
-
 }
